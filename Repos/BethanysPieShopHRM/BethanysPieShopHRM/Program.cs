6298a2b0f5b58140e00bf9fab2f755f3646b17ea
pewmannen <konan.maher@gmail.com>
2025-12-18 13:02:50 +0100

    ps Module 5

--- Repos/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ Repos/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -1,106 +1,91 @@
-﻿Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+﻿
+//int age = 23;
 
-//Console.WriteLine("Please enter your name: ");
+//bool a = age == 23;
 
-//string name = Console.ReadLine();
+//Console.WriteLine("Age is 23: " + a);
 
-//Console.WriteLine("Welcome" + " " +  name + "!");
+//bool b = age >= 23;
+//Console.WriteLine("Age is greater than 23: " + b);
 
+//bool c = (age >= 18) && (age <= 65);
+//Console.WriteLine("Age is between 18 and 65: " + c);
 
-//var monthlyWage = 1234;
+//int age1 = 16;
+//int age2 = 64;
+//bool d = (age1 >= 18) && (age2 <= 65);
+//Console.WriteLine("age1 is greater than 18 AND age2 is less than 65: " + d);
+//bool e = (age1 >= 18) || (age2 <= 65);
+//Console.WriteLine("age1 is greater than 18 AND age2 is less than 65: " + e);
 
-//int months = 12, bonus = 1000;
+//Console.WriteLine("Enter the age of the new candidate: ");
+//int age = int.Parse(Console.ReadLine());
 
-//var isActive = true;
+//if (age < 18)
+//{
+//    Console.WriteLine("Too young to apply");
+//}
+//else if (age > 65)
+//{
+//    Console.WriteLine("Sorry, the selected age is too old");
+//}
+//else
+//{
+//    Console.WriteLine("Great, you can now start with the application!");
+//}
 
-//var rating = 99.25;
-
-//var numberOfEmployees = 300;
-
-//int hoursWorked;
-
-//hoursWorked = 125;
-
-//hoursWorked = 148;
-
-//monthlyWage = true;
-
-//const double interestRate = 0.07;
-
-//interestRate = 0.08;
-
-//string firstName = "Bethany";
-//string lastName = "Smith";
-
-//string emptyString = "";
-
-//Console.WriteLine("Please enter your name");
-//string name = Console.ReadLine();
-
-//string s;
-
-//s = firstName;
-
-
-//double ratePerHour = 12.34;
-//int numberOfHoursWorked = 165;
-
-//double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
-//Console.WriteLine(currentMonthWage);
-
-//ratePerHour += 3; //ratePerHour = ratePerHour + 3
-//Console.WriteLine(ratePerHour);
-
-//if (currentMonthWage > 2000)
-//Console.WriteLine("Top paid employee!");
-
-//int numberOfEmployees = 15;
-//numberOfEmployees--; //++ to increase
-//Console.WriteLine(numberOfEmployees);
-
-//bool a;
-//int b;
-
-//int intMaxValue = int.MaxValue;
-//int intMinValue = int.MinValue;
-
-//char userSelection = 'a';
-
-//char upperVersion = char.ToUpper(userSelection);
-
-//bool isDigit = char.IsDigit(userSelection);
-
-//bool isLetter = char.IsLetter(userSelection);
-
-//var hireDate = new DateTime(2022, 3, 28, 14, 30, 0);
-//Console.WriteLine(hireDate);
-
-//DateTime exitDate = new DateTime(2025, 12, 11);
-
-////DateTime invalidDate = new DateTime(2025, 15, 11); //can't use more than 12 months in a year
-
-//DateTime startDate = hireDate.AddDays(15);
-//Console.WriteLine(startDate);
-
-//DateTime currentDate = DateTime.Now;
-//bool areWeInDst = currentDate.IsDaylightSavingTime();
-
-//DateTime startHour = DateTime.Now;
-//TimeSpan workTime = new TimeSpan(8, 35, 0);
-//DateTime endHour = startHour.Add(workTime);
-
-//Console.WriteLine(startHour.ToLongDateString());
-//Console.WriteLine(endHour.ToShortTimeString());
+//switch (age)
+//{
+//    case < 18:
+//    case > 65:
+//        Console.WriteLine("Sorry, your age is not within the range we are looking for");
+//        break;
+//    case 23:
+//        Console.WriteLine("Wow, exactly what we are looking for");
+//        break;
+//    default:
+//        Console.WriteLine("Great, you can now start with the application!");
+//        break;
+//}
 
 
-//int numberOfHoursWorked = 165;
 
-//long veryLongMonth = numberOfHoursWorked;
 
-//double d = 123456789.0;
+//DateTime today = DateTime.Now;
+//bool endOfMonthPaymentStarted = false;
 
-//int x = (int)d;
+//if (today.Date.Day == 18)
+//{
+//    Console.WriteLine("Please start end-of-month employee payments");
+//}
+//else if (today.Date.Day >= 25 && !endOfMonthPaymentStarted)
+//{
+//    Console.WriteLine("Payments will be late!");
+//}
 
-//int intVeryLongMonth = (int)veryLongMonth;
 
-Console.ReadLine();
+
+
+
+
+Console.WriteLine("Choose the action you want to do: ");
+Console.WriteLine("1. Add employee");
+Console.WriteLine("2. Update employee");
+Console.WriteLine("3. Delete employee");
+string selectionAction = Console.ReadLine();
+
+switch (selectionAction)
+{
+    case "1":
+        Console.WriteLine("Adding new employee...");
+        break;
+    case "2":
+        Console.WriteLine("Updating employee...");
+        break;
+    case "3":
+        Console.WriteLine("Deleting employee");
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        break;
+}
--- Repos/BethanysPieShopHRM/BethanysPieShopHRM/obj/Debug/net8.0/BethanysPieShopHRM.AssemblyInfo.cs
+++ Repos/BethanysPieShopHRM/BethanysPieShopHRM/obj/Debug/net8.0/BethanysPieShopHRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffd6115a43d7e6b2841d3bd8b38774e78b136089")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9791022e5f264e548dfbb17146f119a79e8c4fb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
