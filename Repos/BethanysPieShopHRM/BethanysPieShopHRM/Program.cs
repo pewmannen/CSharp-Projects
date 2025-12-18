ffd6115a43d7e6b2841d3bd8b38774e78b136089
pewmannen <konan.maher@gmail.com>
2025-12-18 11:39:28 +0100

    Init new repository

---
+++ Repos/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -0,0 +1,106 @@
+﻿Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+
+//Console.WriteLine("Please enter your name: ");
+
+//string name = Console.ReadLine();
+
+//Console.WriteLine("Welcome" + " " +  name + "!");
+
+
+//var monthlyWage = 1234;
+
+//int months = 12, bonus = 1000;
+
+//var isActive = true;
+
+//var rating = 99.25;
+
+//var numberOfEmployees = 300;
+
+//int hoursWorked;
+
+//hoursWorked = 125;
+
+//hoursWorked = 148;
+
+//monthlyWage = true;
+
+//const double interestRate = 0.07;
+
+//interestRate = 0.08;
+
+//string firstName = "Bethany";
+//string lastName = "Smith";
+
+//string emptyString = "";
+
+//Console.WriteLine("Please enter your name");
+//string name = Console.ReadLine();
+
+//string s;
+
+//s = firstName;
+
+
+//double ratePerHour = 12.34;
+//int numberOfHoursWorked = 165;
+
+//double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
+//Console.WriteLine(currentMonthWage);
+
+//ratePerHour += 3; //ratePerHour = ratePerHour + 3
+//Console.WriteLine(ratePerHour);
+
+//if (currentMonthWage > 2000)
+//Console.WriteLine("Top paid employee!");
+
+//int numberOfEmployees = 15;
+//numberOfEmployees--; //++ to increase
+//Console.WriteLine(numberOfEmployees);
+
+//bool a;
+//int b;
+
+//int intMaxValue = int.MaxValue;
+//int intMinValue = int.MinValue;
+
+//char userSelection = 'a';
+
+//char upperVersion = char.ToUpper(userSelection);
+
+//bool isDigit = char.IsDigit(userSelection);
+
+//bool isLetter = char.IsLetter(userSelection);
+
+//var hireDate = new DateTime(2022, 3, 28, 14, 30, 0);
+//Console.WriteLine(hireDate);
+
+//DateTime exitDate = new DateTime(2025, 12, 11);
+
+////DateTime invalidDate = new DateTime(2025, 15, 11); //can't use more than 12 months in a year
+
+//DateTime startDate = hireDate.AddDays(15);
+//Console.WriteLine(startDate);
+
+//DateTime currentDate = DateTime.Now;
+//bool areWeInDst = currentDate.IsDaylightSavingTime();
+
+//DateTime startHour = DateTime.Now;
+//TimeSpan workTime = new TimeSpan(8, 35, 0);
+//DateTime endHour = startHour.Add(workTime);
+
+//Console.WriteLine(startHour.ToLongDateString());
+//Console.WriteLine(endHour.ToShortTimeString());
+
+
+//int numberOfHoursWorked = 165;
+
+//long veryLongMonth = numberOfHoursWorked;
+
+//double d = 123456789.0;
+
+//int x = (int)d;
+
+//int intVeryLongMonth = (int)veryLongMonth;
+
+Console.ReadLine();
