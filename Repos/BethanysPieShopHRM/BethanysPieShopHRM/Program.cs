a7280c785858fedc4f0066dd92d43b2a484fcbd6
pewmannen <konan.maher@gmail.com>
2026-02-02 12:01:30 +0100

    Fix: Apply .gitignore to remove build artifacts

--- Repos/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ Repos/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -1,91 +1,11 @@
 ﻿
-//int age = 23;
 
-//bool a = age == 23;
+int amount = 1234;
+int months = 12;
 
-//Console.WriteLine("Age is 23: " + a);
+CalculateYearlyWage(amount, months);
 
-//bool b = age >= 23;
-//Console.WriteLine("Age is greater than 23: " + b);
-
-//bool c = (age >= 18) && (age <= 65);
-//Console.WriteLine("Age is between 18 and 65: " + c);
-
-//int age1 = 16;
-//int age2 = 64;
-//bool d = (age1 >= 18) && (age2 <= 65);
-//Console.WriteLine("age1 is greater than 18 AND age2 is less than 65: " + d);
-//bool e = (age1 >= 18) || (age2 <= 65);
-//Console.WriteLine("age1 is greater than 18 AND age2 is less than 65: " + e);
-
-//Console.WriteLine("Enter the age of the new candidate: ");
-//int age = int.Parse(Console.ReadLine());
-
-//if (age < 18)
-//{
-//    Console.WriteLine("Too young to apply");
-//}
-//else if (age > 65)
-//{
-//    Console.WriteLine("Sorry, the selected age is too old");
-//}
-//else
-//{
-//    Console.WriteLine("Great, you can now start with the application!");
-//}
-
-//switch (age)
-//{
-//    case < 18:
-//    case > 65:
-//        Console.WriteLine("Sorry, your age is not within the range we are looking for");
-//        break;
-//    case 23:
-//        Console.WriteLine("Wow, exactly what we are looking for");
-//        break;
-//    default:
-//        Console.WriteLine("Great, you can now start with the application!");
-//        break;
-//}
-
-
-
-
-//DateTime today = DateTime.Now;
-//bool endOfMonthPaymentStarted = false;
-
-//if (today.Date.Day == 18)
-//{
-//    Console.WriteLine("Please start end-of-month employee payments");
-//}
-//else if (today.Date.Day >= 25 && !endOfMonthPaymentStarted)
-//{
-//    Console.WriteLine("Payments will be late!");
-//}
-
-
-
-
-
-
-Console.WriteLine("Choose the action you want to do: ");
-Console.WriteLine("1. Add employee");
-Console.WriteLine("2. Update employee");
-Console.WriteLine("3. Delete employee");
-string selectionAction = Console.ReadLine();
-
-switch (selectionAction)
+static void CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
 {
-    case "1":
-        Console.WriteLine("Adding new employee...");
-        break;
-    case "2":
-        Console.WriteLine("Updating employee...");
-        break;
-    case "3":
-        Console.WriteLine("Deleting employee");
-        break;
-    default:
-        Console.WriteLine("Invalid input");
-        break;
+    Console.WriteLine($"Yearly wage: {monthlyWage * numberOfMonthsWorked}");
 }
