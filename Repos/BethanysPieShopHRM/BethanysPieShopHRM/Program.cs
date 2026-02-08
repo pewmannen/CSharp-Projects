b9d422d1bc4e7f55138cf5c4bec4a963a5fc1ff8
pewmannen <konan.maher@gmail.com>
2026-02-08 14:18:25 +0100

    Value types and reference types

--- Repos/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ Repos/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -1,10 +1,12 @@
 ﻿
 using BethanysPieShopHRM;
+using System.Text;
+
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1976, 1, 16), 25, EmployeeType.Manager);
+Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30, EmployeeType.Research);
 
 Console.WriteLine("Creating an employee");
 Console.WriteLine("---------------------\n");
-
-Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime (1976, 1, 16), 25);
 
 bethany.DisplayEmployeeDetails();
 
@@ -13,25 +15,8 @@ bethany.PerformWork();
 bethany.PerformWork(5);
 bethany.PerformWork();
 
-bethany.firstName = "Mark";
-bethany.hourlyRate = 10;
-
-bethany.DisplayEmployeeDetails();
-bethany.PerformWork();
-bethany.PerformWork(12);
-bethany.PerformWork();
-bethany.PerformWork();
-
-string fn = bethany.firstName;
-
 double receivedWageBethany = bethany.receiveWage(true);
 Console.WriteLine($"Wage paid (message from program): {receivedWageBethany}\n");
-
-Console.WriteLine("Creating an employee");
-Console.WriteLine("---------------------\n");
-
-//Either do "Employee george = new Employee()" or "Employee george = new()"
-Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30);
 
 george.DisplayEmployeeDetails();
 george.PerformWork();
@@ -42,5 +27,7 @@ george.PerformWork(8);
 
 var receiveWageGeorge = george.receiveWage(true);
 
-Console.WriteLine("\n");
-Console.WriteLine("---------------------");
+WorkTask task;
+task.description = "Baking delicious pies";
+task.hours = 3;
+task.PerformWorkTask();
